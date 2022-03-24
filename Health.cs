c6c204c1a8e59004890e981701448d27c57d0789
seb1419 <92172406+seb1419@users.noreply.github.com>
2022-03-24 12:04:57 -0400

    Add files via upload

---
+++ Health.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth = 15;
+    public float currentHealth;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    
+}
